6610a7df6e662285ccdb79064930deab9aed68a5
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-07-22 20:19:43 -0400

    Update ckg_types.h

--- core/ckg_types.h
+++ core/ckg_types.h
@@ -32,8 +32,8 @@ typedef u8 Boolean;
 #define MegaBytes(value) ((u64)KiloBytes(value) * 1024L)
 #define GigaBytes(value) ((u64)MegaBytes(value) * 1024L)
 
-#define MIN(value, value2) (value > value2 ? value2 : value)
-#define MAX(value, value2) (value < value2 ? value2 : value)
+#define MIN(value, min_value) (value > min_value ? value : min_value)
+#define MAX(value, max_value) (value < max_value ? value : max_value)
 #define CLAMP(value, min_value, max_value) (MAX(MIN(value, min_value), min_value))
 
 #define local_persist static
