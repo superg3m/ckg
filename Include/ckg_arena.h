f6f01415dbb8053b598c13e7864077f14d1d38f7
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-08-14 10:37:45 -0400

    good work

--- Include/ckg_arena.h
+++ Include/ckg_arena.h
@@ -1,20 +1,25 @@
 #pragma once
 
 #include "ckg_types.h"
-
+#include "ckg_linked_list.h"
 //========================== Begin Types ==========================
+typedef struct CKG_ArenaPage {
+	void* base_address;
+	u64 capacity;
+	u64 used;
+} CKG_ArenaPage;
+
 typedef struct CKG_Arena {
 	const char* name;
-    void* base_address;
-    u64 capacity;
-	u64 used;
+	CKG_LinkedList* pages;
 	u32 flag;
+	u8 alignment;
 } CKG_Arena;
 
 typedef enum CKG_ArenaFlag {
-  CKG_ARENA_FLAG_DEFAULT,
+  CKG_ARENA_FLAG_FIXED,
   CKG_ARENA_FLAG_CIRCULAR,
-  CKG_ARENA_FLAG_VECTOR,
+  CKG_ARENA_FLAG_EXTENDABLE_PAGES,
   CKG_ARENA_FLAG_COUNT
 } CKG_ArenaFlag;
 //=========================== End Types ===========================
@@ -23,10 +28,10 @@ typedef enum CKG_ArenaFlag {
 #ifdef __cplusplus
 extern "C" {
 #endif
-	CKG_Arena MACRO_ckg_arena_create(u32 allocation, const char* name, CKG_ArenaFlag flag);
+	CKG_Arena* MACRO_ckg_arena_create(u32 allocation_size, const char* name, CKG_ArenaFlag flag, u8 alignment);
 	void* MACRO_ckg_arena_push(CKG_Arena* arena, u32 element_size);	
 	
-	void ckg_arena_free(CKG_Arena* arena);
+	CKG_Arena* MACRO_ckg_arena_free(CKG_Arena* arena);
 	void ckg_arena_clear(CKG_Arena* arena);
 #ifdef __cplusplus
 }
@@ -34,8 +39,10 @@ extern "C" {
 //************************** End Functions **************************
 
 //+++++++++++++++++++++++++++ Begin Macros ++++++++++++++++++++++++++
-#define ckg_arena_create(allocation_size, name) MACRO_ckg_arena_create(allocation_size, name, CKG_ARENA_FLAG_DEFAULT)
-#define ckg_arena_create_custom(allocation_size, name, flags) MACRO_ckg_arena_create(allocation_size, name, flags)
+#define ckg_arena_create(allocation_size, name) MACRO_ckg_arena_create(allocation_size, name, CKG_ARENA_FLAG_EXTENDABLE_PAGES, 0)
+#define ckg_arena_create_custom(allocation_size, name, flags, alignment) MACRO_ckg_arena_create(allocation_size, name, flags, alignment)
+
+#define ckg_arena_free(arena) arena = MACRO_ckg_arena_free(arena)
 
 #define ckg_arena_push(arena, type) ((type*)MACRO_ckg_arena_push(arena, sizeof(type)))
 #define ckg_arena_push_array(arena, type, element_count) ((type*)MACRO_ckg_arena_push(arena, sizeof(type) * element_count))
@@ -51,50 +58,82 @@ extern "C" {
 		return arena->flag == flag;
 	}
 
-	CKG_Arena MACRO_ckg_arena_create(u32 allocation_size, const char* name, CKG_ArenaFlag flag) {
-		CKG_Arena arena;
-		arena.name = name;
-		arena.flag = flag;
-		arena.capacity = allocation_size;
-		arena.used = 0;
-		arena.base_address = ckg_alloc(allocation_size != 0 ? allocation_size : ARENA_DEFAULT_ALLOCATION_SIZE);
+	internal CKG_ArenaPage* ckg_arena_page_create(u32 allocation_size) {
+		CKG_ArenaPage* ret = ckg_alloc(sizeof(CKG_ArenaPage));
+		ret->used = 0;
+		ret->capacity = allocation_size;
+		ret->base_address = ckg_alloc(allocation_size != 0 ? allocation_size : ARENA_DEFAULT_ALLOCATION_SIZE);
+
+		return ret;
+	}
+
+	CKG_Arena* MACRO_ckg_arena_create(u32 allocation_size, const char* name, CKG_ArenaFlag flag, u8 alignment) {
+		ckg_assert_msg((alignment & 1) == 0, "Arena alignment is not a power of two\n");
+		CKG_Arena* arena = ckg_alloc(sizeof(CKG_Arena));
+		arena->alignment = alignment == 0 ? 8 : alignment;
+		arena->name = name;
+		arena->flag = flag;
+		arena->pages = ckg_linked_list_create(CKG_ArenaPage*, TRUE);
+		CKG_ArenaPage* inital_page = ckg_arena_page_create(allocation_size);
+		ckg_linked_list_push(arena->pages, inital_page);
+
 		return arena;
 	}
 
-	void ckg_arena_free(CKG_Arena* arena) {
-		ckg_assert(arena && arena->base_address);
-		ckg_free(arena->base_address);
+	CKG_Arena* MACRO_ckg_arena_free(CKG_Arena* arena) {
+		ckg_assert(arena);
+
+		for (int i = 0; i < arena->pages->count; i++) {
+			CKG_ArenaPage* page = ckg_linked_list_remove(arena->pages, 0).data;
+			ckg_assert(page->base_address);
+			ckg_free(page->base_address);
+			ckg_free(page);
+		}
+		ckg_free(arena);
+
+		return arena;
 	}
 
 	void ckg_arena_clear(CKG_Arena* arena) {
-		ckg_assert(arena && arena->base_address);
-		ckg_memory_zero(arena->base_address, arena->used);
-		arena->used = 0;
+		ckg_assert(arena);
+
+		for (int i = 0; i < arena->pages->count; i++) {
+			CKG_ArenaPage* page = ckg_linked_list_get(arena->pages, i);
+			ckg_assert(page->base_address);
+			ckg_memory_zero(page->base_address, page->used);
+			page->used = 0;
+		}
 	}
 
 	void* MACRO_ckg_arena_push(CKG_Arena* arena, u32 element_size) {
-		ckg_assert(arena && arena->base_address);
+		ckg_assert(arena);
 
-		if (ckg_CKG_ARENA_FLAG_is_set(arena, CKG_ARENA_FLAG_DEFAULT)) {
-			ckg_assert((arena->used + element_size <= arena->capacity));
-		} else if (ckg_CKG_ARENA_FLAG_is_set(arena, CKG_ARENA_FLAG_CIRCULAR)) {
-			if ((arena->used + element_size > arena->capacity)) {
-				arena->used = 0;
-				ckg_assert((arena->used + element_size <= arena->capacity));
+		CKG_ArenaPage* last_page = ckg_linked_list_peek_tail(arena->pages);
+		if (ckg_CKG_ARENA_FLAG_is_set(arena, CKG_ARENA_FLAG_FIXED)) { // single page assert if you run out of memory
+			ckg_assert((last_page->used + element_size <= last_page->capacity));
+		} else if (ckg_CKG_ARENA_FLAG_is_set(arena, CKG_ARENA_FLAG_CIRCULAR)) { // single page circle around if you run out of memory
+			if ((last_page->used + element_size > last_page->capacity)) {
+				last_page->used = 0;
+				ckg_assert((last_page->used + element_size <= last_page->capacity));
 			}
-		} else if (ckg_CKG_ARENA_FLAG_is_set(arena, CKG_ARENA_FLAG_VECTOR)) {
-			if ((arena->used + element_size > arena->capacity)) {
-				arena->capacity += element_size;
-				arena->capacity *= 2;
-				arena->base_address = ckg_realloc(arena->base_address, arena->capacity, arena->capacity * 2);
-				ckg_assert(arena->base_address);
+		} else if (ckg_CKG_ARENA_FLAG_is_set(arena, CKG_ARENA_FLAG_EXTENDABLE_PAGES)) {
+			ckg_assert(last_page->base_address);
+			if ((last_page->used + element_size > last_page->capacity)) {
+				CKG_ArenaPage* next_page = ckg_arena_page_create((last_page->capacity + element_size) * 2);
+				ckg_assert(next_page->base_address);
+				ckg_linked_list_push(arena->pages, next_page);
 			}
 		} else {
 			ckg_assert(FALSE);
 		}
 
-		u8* ret = ((u8*)arena->base_address) + arena->used;
-		arena->used += element_size;
+		last_page = ckg_linked_list_peek_tail(arena->pages); // tail might change
+
+		u8* ret = ((u8*)last_page->base_address) + last_page->used;
+		last_page->used += element_size;
+		if ((last_page->used & (arena->alignment - 1)) != 0) { // if first bit is set then its not aligned
+			last_page->used += (arena->alignment - (last_page->used & (arena->alignment - 1)));
+		}
 
 		return ret;
 	}
--- example/tests/test_arena_functions.c
+++ example/tests/test_arena_functions.c
@@ -1,15 +1,15 @@
 #include "../../ckg.h"
 
 void test_ckg_arena_operations() {
-  CKG_Arena arena = ckg_arena_create(1024, "Test Arena");
+  CKG_Arena* arena = ckg_arena_create(1024, "Test Arena");
 
   // Test arena_push
-  int* int_ptr = ckg_arena_push(&arena, int);
+  int* int_ptr = ckg_arena_push(arena, int);
   *int_ptr = 5;
   ckg_assert(*int_ptr == 5);
 
   // Test arena_push_array
-  int* int_array = ckg_arena_push_array(&arena, int, 5);
+  int* int_array = ckg_arena_push_array(arena, int, 5);
   for (u32 i = 0; i < 5; i++) {
     int_array[i] = i;
   }
@@ -18,43 +18,40 @@ void test_ckg_arena_operations() {
   }
 
   // Test arena_clear
-  ckg_arena_clear(&arena);
-  ckg_assert(arena.used == 0);
+  ckg_arena_clear(arena);
 
   // Test arena_free
-  ckg_arena_free(&arena);
+  ckg_arena_free(arena);
 
   const int arena_size_common = sizeof(int) * 16;
 
-    // Test default arena
-  CKG_Arena default_arena = ckg_arena_create(arena_size_common, "Default Arena");
-  for (u32 i = 0; i < 16; i++) {
-    int* ptr = ckg_arena_push(&default_arena, int);
+  // Test extendable arena
+  CKG_Arena* extendable_arena = ckg_arena_create(arena_size_common, "Extendable Arena");
+  for (u32 i = 0; i < 32; i++) {
+    int* ptr = ckg_arena_push(extendable_arena, int);
     *ptr = i;
   }
-  ckg_arena_free(&default_arena);
+  ckg_arena_free(extendable_arena);
 
   // Test circular arena
-  CKG_Arena circular_arena = ckg_arena_create_custom(arena_size_common, "Circular Arena", CKG_ARENA_FLAG_CIRCULAR);
+  CKG_Arena* circular_arena = ckg_arena_create_custom(arena_size_common, "Circular Arena", CKG_ARENA_FLAG_CIRCULAR, sizeof(int));
   for (u32 i = 0; i < 32; i++) {
-    int* ptr = ckg_arena_push(&circular_arena, int);
+    int* ptr = ckg_arena_push(circular_arena, int);
     *ptr = i;
   }
-  ckg_assert(circular_arena.used == arena_size_common);
-  int* first_ptr = circular_arena.base_address;
-  int* last_ptr = ckg_arena_push(&circular_arena, int);
+
+  int* last_ptr = ckg_arena_push(circular_arena, int);
   *last_ptr = 42;
-  ckg_assert(*first_ptr == 42);
-  ckg_arena_free(&circular_arena);
+  ckg_arena_free(circular_arena);
 
   // Test vector arena
-  CKG_Arena vector_arena = ckg_arena_create_custom(arena_size_common, "Vector Arena", CKG_ARENA_FLAG_VECTOR);
-  for (u32 i = 0; i < 32; i++) {
-    int* ptr = ckg_arena_push(&vector_arena, int);
+  CKG_Arena* fixed_arena = ckg_arena_create_custom(arena_size_common, "Fixed Arena", CKG_ARENA_FLAG_FIXED, sizeof(int));
+  for (u32 i = 0; i < 16; i++) {
+    int* ptr = ckg_arena_push(fixed_arena, int);
     *ptr = i;
   }
-  ckg_assert(vector_arena.used > arena_size_common);
-  ckg_arena_free(&vector_arena);
+
+  ckg_arena_free(fixed_arena);
 
   CKG_LOG_SUCCESS("All arena tests passed!\n");
   return;
