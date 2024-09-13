18a37f97e63949ede286e18d47b3d214c2e1526c
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-13 08:47:48 -0400

    Update test_ckg.c

--- example/test_ckg.c
+++ example/test_ckg.c
@@ -101,8 +101,6 @@ int main() {
 
 	ckg_assert(GET_BIT(number_to_endian_swap, 0));
 
-	ckg_assert(FALSE);
-
 	CKG_LOG_WARN("================================ THIS WORKS ALL THE WAY I THINK! CKG END ================================\n");
 	return 0;
 }
