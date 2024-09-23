99d9c7717151dc167f8f3dbcfab998c5b2346ec2
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-09-22 22:07:10 -0400

    ok

--- ckg.h
+++ ckg.h
@@ -1,11 +1,198 @@
 #pragma once
 
 // unify this to be a single file
+#if defined(CKG_EXTERN)
+    #define CKG_API extern
+#else
+    #define CKG_API
+#endif
 
-#include "./Include/ckg_types.h"
-#include "./Include/ckg_stack_trace.h"
-#include "./Include/ckg_assert.h"
-#include "./Include/ckg_arena.h"
+#if defined (CKG_INCLUDE_ALL)
+    #define CKG_INCLUDE_TYPES
+    #define CKG_INCLUDE_ARENA
+    #define CKG_INCLUDE_CSTRING
+    #define CKG_INCLUDE_CHAR
+    #define CKG_INCLUDE_COLLECTIONS
+    #define CKG_INCLUDE_PLATFORM
+    #define CKG_INCLUDE_ASSERT
+    #define CKG_INCLUDE_LOGGER
+#endif
+
+#if defined (CKG_IMPL_ALL)
+    #define CKG_IMPL_TYPES
+    #define CKG_IMPL_ARENA
+    #define CKG_IMPL_CSTRING
+    #define CKG_IMPL_CHAR
+    #define CKG_IMPL_COLLECTIONS
+    #define CKG_IMPL_PLATFORM
+    #define CKG_IMPL_ASSERT
+    #define CKG_IMPL_LOGGER
+#endif
+
+#if define(CKG_INCLUDE_TYPES)
+    #include <stdint.h>
+    #include <stdio.h>
+    #include <stdarg.h>
+    #include <stdlib.h>
+
+    typedef int8_t  s8;
+    typedef int16_t s16;
+    typedef int32_t s32;
+    typedef int64_t s64;
+
+    typedef uint8_t  u8;
+    typedef uint16_t u16;
+    typedef uint32_t u32;
+    typedef uint64_t u64;
+
+    typedef u8 Boolean;
+
+    #define FALSE 0
+    #define TRUE 1
+    #define NULLPTR 0
+    #define PI 3.14159265359
+
+    #define stringify(entry) #entry
+    #define glue(a, b) a##b
+
+    #define KiloBytes(value) ((u64)(value) * 1024L)
+    #define MegaBytes(value) ((u64)KiloBytes(value) * 1024L)
+    #define GigaBytes(value) ((u64)MegaBytes(value) * 1024L)
+
+    #define MIN(a, b) (((a) < (b)) ? (a) : (b))
+    #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+    #define CLAMP(value, min_value, max_value) (MIN(MAX(value, min_value), max_value))
+
+    #define local_persist static
+    #define internal static
+
+    // Date: July 12, 2024
+    // TODO(Jovanni): Test this to make sure its actually works but it makes sense to me
+    #define OFFSET_OF(type, member) sizeof((u64)(&(((type*)0)->member)))
+
+    #define FIRST_DIGIT(number) ((int)number % 10);
+
+    CKG_API void U32_EndianSwap(u32* number_to_endian_swap);
+    CKG_API void U64_EndianSwap(u64* number_to_endian_swap);
+    #define GET_BIT(number, bit_to_check) ((number & (1 << bit_to_check)) >> bit_to_check)
+    #define SET_BIT(number, bit_to_set) number |= (1 << bit_to_set);
+    #define UNSET_BIT(number, bit_to_unset) number &= (~(1 << bit_to_unset));
+
+    #define ArrayCount(array) (sizeof(array) / sizeof(array[0]))
+
+    #define PLATFORM_MAX_PATH 256
+
+    #if defined(_WIN32)
+        #define PLATFORM_WINDOWS
+        #define OS_DELIMITER '\\'
+        #define CRASH __debugbreak()
+    #elif defined(__APPLE__)
+        #define PLATFORM_APPLE
+        #define OS_DELIMITER '/'
+        #define CRASH __builtin_trap()
+    #elif defined(__linux__) || defined(__unix__) || defined(__POSIX__)
+        #define PLATFORM_LINUX
+        #define OS_DELIMITER '/'
+        #define CRASH __builtin_trap()
+    #else
+        #error "Unknown Platform???"
+    #endif
+
+    CKG_API void ckg_stack_trace_dump();
+#endif
+
+#if define(CKG_INCLUDE_LOGGER)
+    //Regular text
+    #define CKG_BLACK "\033[0;30m"
+    #define CKG_RED "\033[0;31m"
+    #define CKG_GREEN "\033[0;32m"
+    #define CKG_YELLOW "\033[0;33m"
+    #define CKG_BLUE "\033[0;34m"
+    #define CKG_PURPLE "\033[0;35m"
+    #define CKG_CYAN "\033[0;36m"
+
+    //Regular background
+    #define CKG_BLACK_BACKGROUND "\033[40m"
+    #define CKG_RED_BACKGROUND "\033[41m"
+    #define CKG_GREENN_BACKGROUND "\033[42m"
+    #define CKG_YELLOW_BACKGROUND "\033[43m"
+    #define CKG_BLUE_BACKGROUND "\033[44m"
+    #define CKG_PURPLE_BACKGROUND "\033[45m"
+    #define CKG_CYAN_BACKGROUND "\033[46m"
+    #define CKG_COLOR_RESET "\033[0m"
+
+    #define CKG_LOG_LEVEL_CHARACTER_LIMIT 15
+    #define CKG_PLATFORM_CHARACTER_LIMIT 512
+
+    // Log Levels
+    #define LOG_LEVEL_FATAL 0
+    #define LOG_LEVEL_ERROR 1
+    #define LOG_LEVEL_WARN 2
+    #define LOG_LEVEL_DEBUG 3
+    #define LOG_LEVEL_SUCCESS 4
+    #define LOG_LEVEL_PRINT 5
+    #define LOG_LEVEL_COUNT 6
+    typedef u8 CKG_LogLevel;
+
+    CKG_API void MACRO_ckg_log_output(CKG_LogLevel log_level, const char* message, ...);
+    #define ckg_log_output(log_level, message, ...) MACRO_ckg_log_output(log_level, message, ##__VA_ARGS__)
+    #define CKG_LOG_PRINT(message, ...) ckg_log_output(LOG_LEVEL_PRINT, message, ##__VA_ARGS__)
+    #define CKG_LOG_SUCCESS(message, ...) ckg_log_output(LOG_LEVEL_SUCCESS, message, ##__VA_ARGS__)
+    #define CKG_LOG_DEBUG(message, ...) ckg_log_output(LOG_LEVEL_DEBUG, message, ##__VA_ARGS__)
+    #define CKG_LOG_WARN(message, ...) ckg_log_output(LOG_LEVEL_WARN, message, ##__VA_ARGS__)
+    #define CKG_LOG_ERROR(message, ...) ckg_log_output(LOG_LEVEL_ERROR, message, ##__VA_ARGS__)
+    #define CKG_LOG_FATAL(message, ...) ckg_log_output(LOG_LEVEL_FATAL, message, ##__VA_ARGS__)
+#endif
+
+
+#if defined(CKG_INCLUDE_ASSERT)
+    #define CKG_ASSERT_ENABLED TRUE
+    #if CKG_ASSERT_ENABLED == TRUE	   
+        #define ckg_assert(expression)                            \
+        do {                                                      \
+            if (!(expression)) {                                  \
+                ckg_stack_trace_dump();                           \
+                char msg[] = "Func: %s, File: %s:%d\n";           \
+                CKG_LOG_FATAL(msg, __func__, __FILE__, __LINE__); \
+                CRASH;                                            \
+            }                                                     \
+        } while (FALSE)
+
+        #define ckg_assert_msg(expression, message, ...)	\
+            do {                                            \
+                if (!(expression)) {                        \
+                    ckg_stack_trace_dump();                 \
+                    CKG_LOG_FATAL(message, ##__VA_ARGS__);  \
+                    CRASH;                                  \
+                }                                           \
+            } while (FALSE)
+    #else
+            #define ckg_assert(expression)
+            #define ckg_assert_msg(expression, message, ...)
+    #endif
+#endif
+
+#if defined(CKG_INCLUDE_ARENA)
+    #define CKG_ARENA_FLAG_FIXED 0
+    #define CKG_ARENA_FLAG_CIRCULAR 1
+    #define CKG_ARENA_FLAG_EXTENDABLE_PAGES 2
+    #define CKG_ARENA_FLAG_COUNT 3
+    typedef u8 CKG_ArenaFlag;
+    typedef struct CKG_Arena CKG_Arena;
+
+
+    CKG_API CKG_Arena* MACRO_ckg_arena_create(size_t allocation_size, const char* name, CKG_ArenaFlag flag, u8 alignment);
+    CKG_API void* MACRO_ckg_arena_push(CKG_Arena* arena, size_t element_size);	
+    CKG_API CKG_Arena* MACRO_ckg_arena_free(CKG_Arena* arena);
+    CKG_API void ckg_arena_clear(CKG_Arena* arena);
+
+    #define ckg_arena_create(allocation_size, name) MACRO_ckg_arena_create(allocation_size, name, CKG_ARENA_FLAG_EXTENDABLE_PAGES, 0)
+    #define ckg_arena_create_custom(allocation_size, name, flags, alignment) MACRO_ckg_arena_create(allocation_size, name, flags, alignment)
+    #define ckg_arena_free(arena) arena = MACRO_ckg_arena_free(arena)
+    #define ckg_arena_push(arena, type) ((type*)MACRO_ckg_arena_push(arena, sizeof(type)))
+    #define ckg_arena_push_array(arena, type, element_count) ((type*)MACRO_ckg_arena_push(arena, sizeof(type) * element_count))
+#endif
+
 #include "./Include/ckg_memory.h"
 #include "./Include/ckg_cstring.h"
 #include "./Include/ckg_char.h"
