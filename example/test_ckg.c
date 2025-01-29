f927351ff536c6b27291dd45232613886e774a88
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-01-29 10:56:18 -0500

    prob gonna need a string view for ckit.
    - String views are by there very nature very short lived so I don't have to worry about invalidating them for the most part.

--- example/test_ckg.c
+++ example/test_ckg.c
@@ -5,18 +5,6 @@
 int main() {
     // The goal is the never worry about null terminator
 
-    char* str = "---- HEY_YOU ----";
-
-    CKG_StringView s1 = ckg_strview_create(str, 5, 12);
-    CKG_StringView s2 = CKG_SV_LIT("HEY_YOU");
-
-    ckg_assert(ckg_cstr_equal(CKG_SV_ARG(s1), CKG_SV_ARG(s2)));
-    ckg_assert(ckg_cstr_contains(CKG_SV_ARG(s1), CKG_SV_ARG(s2)));
-    
-
-    CKG_LOG_PRINT("HELLO: %s\n", ckg_strview_to_cstr(s1));
-    CKG_LOG_PRINT("HELLO: %lld\n", ckg_cstr_index_of(CKG_SV_ARG(s1), CKG_LIT_ARG("HEY_YOU")));
-
     test_ckg_cstr_length();
     test_ckg_string_equal();
     test_ckg_cstr_insert_char();
