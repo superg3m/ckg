8edd872a53bdc961c0eaa339bcd8ab9f4fdab3e3
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-08-29 15:38:54 -0400

    endian stuff

--- ckg.h
+++ ckg.h
@@ -73,10 +73,12 @@
 
     typedef int8_t  s8;
     typedef int16_t s16;
+    typedef int16_t s32;
     typedef int64_t s64;
 
     typedef uint8_t  u8;
     typedef uint16_t u16;
+    typedef uint16_t u32;
     typedef uint64_t u64;
 
     #define NULLPTR 0
@@ -136,6 +138,8 @@
         #define UNUSED_FUNCTION
     #endif
 
+    CKG_API void U32_EndianSwap(u32* number_to_endian_swap);
+    CKG_API void U64_EndianSwap(u64* number_to_endian_swap);
     CKG_API void ckg_stack_trace_dump(const char* function, const char* file, int line);
 #endif
 
@@ -809,6 +813,32 @@
 //
 
 #if defined(CKG_IMPL_TYPES)
+    void U32_EndianSwap(u32* number_to_endian_swap) {
+        u32 temp = *number_to_endian_swap;
+        
+        u32 b0 = (temp >> 0) & 0xFF;
+        u32 b1 = (temp >> 8) & 0xFF;
+        u32 b2 = (temp >> 16) & 0xFF;
+        u32 b3 = (temp >> 24) & 0xFF;
+
+        *number_to_endian_swap = (b0 << 24)|(b1 << 16)|(b2 << 8)|(b3 << 0);
+    } 
+
+    void U64_EndianSwap(u64* number_to_endian_swap) {
+        u64 temp = *number_to_endian_swap;
+        
+        u64 b0 = (temp >> 0) & 0xFF;
+        u64 b1 = (temp >> 8) & 0xFF;
+        u64 b2 = (temp >> 16) & 0xFF;
+        u64 b3 = (temp >> 24) & 0xFF;
+        u64 b4 = (temp >> 32) & 0xFF;
+        u64 b5 = (temp >> 40) & 0xFF;
+        u64 b6 = (temp >> 48) & 0xFF;
+        u64 b7 = (temp >> 56) & 0xFF;
+
+        *number_to_endian_swap = (b0 << 56)|(b1 << 48)|(b2 << 40)|(b3 << 32)|(b4 << 24)|(b5 << 16)|(b6 << 8)|(b7 << 0);
+    }
+
     #if defined(PLATFORM_WINDOWS) && defined(_MSC_VER)
         #include <DbgHelp.h>
         #pragma comment(lib, "dbghelp")
